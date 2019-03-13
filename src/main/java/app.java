9092f2ad21ba991ead8d79a16189c81ab6292b82
Michal Kaczkowski <sirmichal23@gazeta.pl>
2019-03-13 02:10:16 +0100

    Wersja z drukowaniem

--- src/main/java/app.java
+++ src/main/java/app.java
@@ -2,18 +2,24 @@
 // importowane klasy
 import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.*;
+
+import javax.print.*;
+import javax.print.attribute.HashPrintRequestAttributeSet;
+import javax.print.attribute.PrintRequestAttributeSet;
+import javax.print.attribute.standard.Sides;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 // wykonywalna klasa
 public class app {
-    public static void main(String[] args) throws IOException, DocumentException {
+    public static void main(String[] args) throws IOException, DocumentException, PrintException {
         // wczytywanie oryginalnego dokumentu Pdf, args [0] oznacza tekstowy parametr nr 1. Jest to w naszym przypadku
         // sciezka do oryginalnego pliku Pdf. Ten parametr podajemy w kodzie VBA
         PdfReader oryginalnyPdf = new PdfReader(args[0]);
         // tworzenie kopii oryginalnego Pdf, args [1] to scieżka gdzie ma się ten plik znajdować
         PdfStamper nowyPdf = new PdfStamper(oryginalnyPdf,new FileOutputStream(args[1]));
         // ustawiamy jaki rodzaj czcionki i wielkosc ma miec znak wodny
-        Font czcionka = new Font(Font.FontFamily.HELVETICA,36);
+        Font czcionka = new Font(Font.FontFamily.TIMES_ROMAN,12);
         // budowanie znaku wodnego, args [2] określa jaką treść ma mieć znak wodny. Parametr podawany w VBA
         Phrase znakWodny = new Phrase(args[2],czcionka);
         // pętla, która przechodzi przez wszystkie strony dokumentu i umieszcza znak wodny
@@ -29,12 +35,36 @@ public class app {
             stronaPdf.setGState(ustawienieWizualneStronyPdf);
             // łączymy utworzony wcześniej znak wodny ze stroną pdf. Tutaj można dokładnie ustawić nachylenie,
             // i pozycje tego znaku
-            ColumnText.showTextAligned(stronaPdf, Element.ALIGN_CENTER,znakWodny,297,550,45);
+
+            ColumnText.showTextAligned(stronaPdf, Element.ALIGN_CENTER,znakWodny,580,500,90);
             // zapis
             stronaPdf.restoreState();
         }
         // zamknięcie obu plików pdf
         oryginalnyPdf.close();
         nowyPdf.close();
+
+       // drukowanie
+
+        PrintService drukarkaDomyslna=null;
+        DocFlavor flavor = DocFlavor.SERVICE_FORMATTED.PAGEABLE;
+        PrintRequestAttributeSet listaAtrybutow = new HashPrintRequestAttributeSet();
+        listaAtrybutow.add(Sides.ONE_SIDED);
+        PrintService[] listaDrukarek = PrintServiceLookup.lookupPrintServices(flavor, listaAtrybutow);
+        if (listaDrukarek.length == 0) {
+            throw new IllegalStateException("Nie wykryto drukarek");
+        }
+        for (int i = 0; i <listaDrukarek.length ; i++) {
+            if(listaDrukarek[i].getName().equals(PrintServiceLookup.lookupDefaultPrintService().getName())){
+                drukarkaDomyslna=listaDrukarek[i];
+            }
+        }
+
+
+        FileInputStream pdfDoWydruku = new FileInputStream(args[1]);
+        Doc pdfDoc = new SimpleDoc(pdfDoWydruku, DocFlavor.INPUT_STREAM.AUTOSENSE, null);
+        DocPrintJob printJob = drukarkaDomyslna.createPrintJob();
+        printJob.print(pdfDoc, new HashPrintRequestAttributeSet());
+        pdfDoWydruku.close();
     }
 }
